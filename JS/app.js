53364a29cbb6162853f974a8c5b35b6025b493a0
DevNetBound <93487441+DevNetBound@users.noreply.github.com>
2023-08-26 15:46:37 -0400

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -43,7 +43,7 @@ particlesJS('particles-js',
         "random": true,
         "anim": {
           "enable": false,
-          "speed": 40,
+          "speed": 70,
           "size_min": 0.1,
           "sync": false
         }
